d69b9ae7f1c0ffe1d4a1c5f8c3120ae5d3883df7
Adriana Babakanian <ababakanian@berkeley.edu>
2017-12-30 12:05:01 -0800

    implemented nav bar from section pages

--- js/app.js
+++ js/app.js
@@ -1,6 +1,17 @@
 $(document).ready(function(){
+
     $('.dropdown').click(function() {
         $('.dropdown-content').toggle();
         $(".bar").toggleClass("act");
+    });
+
+    $('.dropdown_alt').click(function() {
+        $('.dropdown-content_alt').toggle();
+        $(".bar_alt").toggleClass("act_alt");
+        $(".home").toggleClass("hide");
+    });
+
+    $('.home').click(function() {
+        $(".dropdown_alt").toggleClass("hide");
     });
 });
