8915d77ab7879d6c8c36a0ee8238340ca6928d51
Adriana Babakanian <ababakanian@berkeley.edu>
2017-12-30 22:48:00 -0800

    updated navigation ui

--- js/app.js
+++ js/app.js
@@ -1,5 +1,13 @@
 $(document).ready(function(){
 
+
+    $(document).ready(function() {
+      $('.fade').stop(true).fadeIn({
+        duration: 1000,
+        queue: false
+      }).css('display', 'none').slideDown(1000, "linear");
+    });
+
     $('.dropdown').click(function() {
         $('.dropdown-content').toggle();
         $(".bar").toggleClass("act");
