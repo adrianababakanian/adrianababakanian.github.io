052dfddae439cd90d10299b4c680ba41917381dc
Adriana Babakanian <ababakanian@berkeley.edu>
2018-01-01 21:31:39 -0800

    updated prototyping content

--- js/app.js
+++ js/app.js
@@ -1,5 +1,21 @@
+
+
 $(document).ready(function(){
 
+
+
+    function processAjaxData(response, urlPath){
+       document.getElementById("content").innerHTML = response.html;
+       document.title = response.pageTitle;
+       window.history.pushState({"html":response.html,"pageTitle":response.pageTitle},"", urlPath);
+    }
+
+     window.onpopstate = function(e){
+        if(e.state){
+            document.getElementById("content").innerHTML = e.state.html;
+            document.title = e.state.pageTitle;
+        }
+    };
 
     $(document).ready(function() {
       $('.fade').stop(true).fadeIn({
